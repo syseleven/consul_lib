018c753a75cd00beeb775b563133b5976f305ac9
Dennis Kuhn <d.kuhn@syseleven.de>
2017-03-17 14:19:46 +0000

    Check Consul services on all nodes
    The Consul service checks are checked on all consul nodes, not only where
    the agent is running.

--- consul_lib/services.py
+++ consul_lib/services.py
@@ -3,21 +3,36 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-def get_relevant_checks(con, tag):
-    services = con.agent.services()
-    if not services:
-        LOG.debug("Unable to find services.")
-        return None
-    for service in services.values():
-        tags = service["Tags"]
-        if tags and tag in tags:
-            yield service["ID"]
+def get_local_checks(con, tags=[]):
+    """
+    Returns a list with all checks for this agent (local checks).
+    If tags is provided only checks are returned which have at least on tag in common.
+    """
+
+    if tags:
+        checks = [k for k, v in con.agent.services().items() if v["Tags"] and set(tags) | (set(v["Tags"]))]
+    else:
+        checks = con.agent.services().keys()
+
+    if checks:
+        LOG.debug("Found this consul checks: %s" % checks)
+    else:
+        LOG.debug("No consul checks found")
+    return checks
 
 
-def get_failed_checks(con, relevant_checks):
-    checks = con.agent.checks()
-    if not checks:
-        return None
-    for check in checks.values():
-        if check["ServiceID"] in relevant_checks and check["Status"] != "passing":
-            yield check
+def get_failed_cluster_checks(con, checks):
+    """
+    Returns a dict with all checks where the check status is != passing.
+    The checks parameter is a list of service names
+    """
+
+    failed_checks = {}
+    for service in checks:
+        for node in con.health.service(service)[1]:
+            for check in node["Checks"]:
+                LOG.info("Check %s on %s. Status: %s" % (check["CheckID"], check["Node"], check["Status"]))
+                if check["Status"] != "passing":
+                    failed_checks[check["CheckID"]]=check
+
+    return failed_checks
