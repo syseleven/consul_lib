39f5094f9ddb4178dac66364fa0861e5776cd763
Olaf Seibert <o.seibert@syseleven.de>
2020-03-12 16:56:40 +0100

    Rename some variables and make log message more useful.
    Improve a comment too.

--- consul_lib/services.py
+++ consul_lib/services.py
@@ -21,18 +21,23 @@ def get_local_checks(con, tags=[]):
     return checks
 
 
-def get_failed_cluster_checks(con, checks):
+def get_failed_cluster_checks(con, service_names):
     """
     Returns a dict with all checks where the check status is != passing.
-    The checks parameter is a list of service names
+    The service_names parameter is a list of service names.
+
+    If a service has the tag "ignore_maintenance", we ignore the check called
+    "_node_maintenance", which is a failing check when the node is in
+    maintenance.
     """
 
     failed_checks = {}
-    for service in checks:
-        for node in con.health.service(service)[1]:
-            tags = node["Service"]["Tags"]
-            for check in node["Checks"]:
-                LOG.info("Check %s on %s. Status: %s" % (check["CheckID"], check["Node"], check["Status"]))
+    for service_name in service_names:
+        for service_results in con.health.service(service_name)[1]:
+            tags = service_results["Service"]["Tags"]
+            for check in service_results["Checks"]:
+                LOG.info("Service %s check %s on %s. Status: %s",
+                         service_name, check["CheckID"], check["Node"], check["Status"])
                 if check["Status"] != "passing":
                     if not ignore_maintenance_check(check["CheckID"], tags):
                         failed_checks[check["CheckID"]] = check
